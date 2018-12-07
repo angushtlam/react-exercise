3317ad66c0f1699479a3f4ef1f72917d7fdd3fc4
angus <angushtlam@gmail.com>
2018-12-07 10:19:23 -0500

    Updates

---
+++ precache-manifest.74c4d426f3da3e10ed4ea2cc85c0e435.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4a7ac28972581575ae9414a41bde6e8",
+    "url": "/react-exercise/static/media/storage_move.d4a7ac28.svg"
+  },
+  {
+    "revision": "a2cc7dc79630f7daa360",
+    "url": "/react-exercise/static/js/main.a2cc7dc7.chunk.js"
+  },
+  {
+    "revision": "067355a175a395c13cbf",
+    "url": "/react-exercise/static/js/runtime~main.067355a1.js"
+  },
+  {
+    "revision": "b20677bcb4ef77c4598215d8a5e8b299",
+    "url": "/react-exercise/static/media/logo.b20677bc.svg"
+  },
+  {
+    "revision": "269ad6678f0a7170fad92a3031668a11",
+    "url": "/react-exercise/static/media/craigslist_pickup.269ad667.svg"
+  },
+  {
+    "revision": "2b4d487a6e01089d2adb7563bc6e8ffe",
+    "url": "/react-exercise/static/media/donation_pickup.2b4d487a.svg"
+  },
+  {
+    "revision": "c1d45024986d5f8aeced0ae422508e21",
+    "url": "/react-exercise/static/media/junk_removal.c1d45024.svg"
+  },
+  {
+    "revision": "0c6e30fd5fd04894fdce9cbc297819bc",
+    "url": "/react-exercise/static/media/other.0c6e30fd.svg"
+  },
+  {
+    "revision": "7dd66e287035625dfcb7e5e11dc24054",
+    "url": "/react-exercise/static/media/small_move.7dd66e28.svg"
+  },
+  {
+    "revision": "00e9ce6a6bbb1b865060d3736a237066",
+    "url": "/react-exercise/static/media/van.00e9ce6a.svg"
+  },
+  {
+    "revision": "31b106e10585f1119c429d8c9da1ce2c",
+    "url": "/react-exercise/static/media/store_delivery.31b106e1.svg"
+  },
+  {
+    "revision": "aed59585a56b0868b588c28a56e941be",
+    "url": "/react-exercise/static/media/down_arrow.aed59585.svg"
+  },
+  {
+    "revision": "80b6c57a1e2ee6fe093704388f142a74",
+    "url": "/react-exercise/static/media/up_arrow.80b6c57a.svg"
+  },
+  {
+    "revision": "b2af850cd070bc90e47d3dc488b9e44d",
+    "url": "/react-exercise/static/media/all_our_luggers.b2af850c.svg"
+  },
+  {
+    "revision": "e8247ef302bc6ec63b4f729ed3d039cc",
+    "url": "/react-exercise/static/media/one_lugger.e8247ef3.svg"
+  },
+  {
+    "revision": "9e318d5b55db47a7576f152870b7c4f9",
+    "url": "/react-exercise/static/media/two_luggers.9e318d5b.svg"
+  },
+  {
+    "revision": "49cebe89f4e5a4cc5ff19d957aa2e0ad",
+    "url": "/react-exercise/static/media/pickup.49cebe89.svg"
+  },
+  {
+    "revision": "94fe24dcd7ebc5fb9795",
+    "url": "/react-exercise/static/js/1.94fe24dc.chunk.js"
+  },
+  {
+    "revision": "78da2ff80721cd414cb8171c9b9d8580",
+    "url": "/react-exercise/index.html"
+  }
+];
